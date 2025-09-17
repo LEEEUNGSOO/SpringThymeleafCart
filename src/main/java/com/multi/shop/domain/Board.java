a17a0dd3dd7da47121a83d63bd52a6af9cf2e254
이응수 <sistsay@gmail.com>
2025-09-17 13:01:33 +0900

    board

---
+++ src/main/java/com/multi/shop/domain/Board.java
@@ -0,0 +1,15 @@
+package com.multi.shop.domain;
+
+
+
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class Board {
+    private int id;
+    private String title;
+    private String content;
+    private String writer;
+    private LocalDateTime createdAt;
+}
